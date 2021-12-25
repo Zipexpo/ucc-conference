ad0eebedf57800e57c6fe7e1d3e166f81ebe69a6
Zipexpo <nvtngan@hcmus.edu.vn>
2021-12-25 02:40:19 -0600

    update

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { withRouter, Switch } from 'react-router-dom';
+import AppRoute from './utils/AppRoute';
+import ScrollReveal from './utils/ScrollReveal';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// Layouts
+import LayoutDefault from './layouts/LayoutDefault';
+import LayoutAlternative from './layouts/LayoutAlternative';
+import LayoutSignin from './layouts/LayoutSignin';
+
+// Views 
+import Home from './views/Home';
+import Secondary from './views/Secondary';
+import Login from './views/Login';
+import Signup from './views/Signup';
+
+class App extends React.Component {
+
+  componentDidMount() {
+    document.body.classList.add('is-loaded')
+    this.refs.scrollReveal.init();
+  }
+
+  // Route change
+  componentDidUpdate(prevProps) {
+    if (this.props.location.pathname !== prevProps.location.pathname) {
+      this.refs.scrollReveal.init();
+    }
+  }
+
+  render() {
+    return (
+      <ScrollReveal
+        ref="scrollReveal"
+        children={() => (
+          <Switch>
+            <AppRoute exact path="/" component={Home} layout={LayoutDefault} />
+            <AppRoute exact path="/home" component={Home} layout={LayoutDefault} />
+            <AppRoute exact path="/secondary" component={Secondary} layout={LayoutAlternative} />
+            <AppRoute exact path="/secondary" component={Secondary} layout={LayoutAlternative} />
+            <AppRoute exact path="/login" component={Login} layout={LayoutSignin} />
+            <AppRoute exact path="/signup" component={Signup} layout={LayoutSignin} />
+          </Switch>
+        )} />
+    );
+  }
 }
 
-export default App;
+export default withRouter(props => <App {...props} />);
